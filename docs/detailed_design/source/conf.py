24b4a4c321b1551359b3734d3b8a8600043ca03b
Devin Schwab <digidevin@gmail.com>
2012-10-23 21:55:01 -0400

    Added the sphinx inheritance diagram extension

--- docs/detailed_design/source/conf.py
+++ docs/detailed_design/source/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('../../../src'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -245,3 +245,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+
+# -- Options for Inheritance diagram
