660125f54efe850afb99114cf967d7441024fa74
Devin Schwab <digidevin@gmail.com>
2012-11-02 02:38:49 -0400

    Added the findUsers method along with unit tests. All unittests pass and pylint gives perfect scores to all files

--- src/application/accounts/accounts.py
+++ src/application/accounts/accounts.py
@@ -15,6 +15,8 @@ from application.accounts.models import UserModel
 from Crypto.Hash import SHA # used with the passwords
 from application.generate_keys import generate_randomkey
 
+from google.appengine.ext.db import BadValueError
+
 class User(login.UserMixin):
     """This call is the main class used for accounts.
     It contains all of the information about a specific
@@ -31,6 +33,7 @@ class User(login.UserMixin):
     # add more attribute names here to make them
     # nonmodifiable via __setattr__
     non_modifiable_attr = ['hash', 'salt']
+    non_accessible_attr = ['hash', 'salt']
     
     def __init__(self): # pylint: disable=W0231
         pass
@@ -40,6 +43,26 @@ class User(login.UserMixin):
             raise AttributeError('%s is nonmodifiable' % name)
         else:
             self.__dict__[name] = value
+
+    def __eq__(self, obj):
+        """Override of __eq__ method.
+
+        Returns True if the keys are equal
+        Otherwise returns False
+        """
+        try:
+            if self.key() == obj.key():
+                return True
+            else:
+                return False
+        # if the object turns out to be of a different type
+        except AttributeError:
+            return False
+
+    def key(self):
+        """Wrapper method to access internal UserModel's
+        key() method"""
+        return self.__UserModel.key() # pylint: disable=E1101
 
 def create_user(fname, lname, cwruid, password, **kwargs):
     """This method is a factory method for User accounts.
@@ -96,10 +119,79 @@ def create_user(fname, lname, cwruid, password, **kwargs):
         # from outside of this function
         if key in User.non_modifiable_attr:
             raise AttributeError
-        user_model.__setattr__(key, kwargs[key])
+        try:
+            user_model.__setattr__(key, kwargs[key])
+        except BadValueError:
+            raise AttributeError("%s has invalid type" % key)
 
     user_model.put()
 
     new_user = User()
     new_user._User__UserModel = user_model # pylint: disable=C0103,W0201
     return new_user
+
+def find_users(limit=None, **kwargs):
+    """This method is used to retrieve already created User entities.
+    It queries the database for all users matching the attributes in
+    kwargs.
+
+    limit when None means all matching users will be returned. If limit
+    is set to a number then all users matching the query will be returned
+    up to the amount specified by limit.
+
+    For each user found an instance of accounts.accounts.User is created
+    and added to a list. This list is returned at the end of the method.
+
+    If no users are found the list will be empty.
+
+    Query parameters will be specified by tuples. The first item in a tuple
+    will be the search operator e.g. '=' and the second item will be
+    the value for that search operator e.g. 'Smith'
+
+    So to search for a list of Users with last name of 'Smith' and
+    first name of 'John' the function call would be
+
+    findUsers(lname=('=','Smith'),fname=('=','John'))
+    """
+    if(limit is not None and limit <= 0):
+        raise TypeError("limit must be a number greater than 0 or None")
+    
+    query = UserModel.all()
+
+    # for each kwarg filter the query
+    for key in kwargs:
+        # check if the UserModel has such an attribute
+        if not hasattr(UserModel, key):
+            raise AttributeError("%s attribute doesn't exist" % key)
+        # check if the attribute is accessible
+        if key in User.non_accessible_attr:
+            raise AttributeError("%s attribute is not accessible" % key)
+
+        try:
+            query.filter('%s %s' % (key, kwargs[key][0]), kwargs[key][1])
+        except IndexError:
+            raise TypeError("%s=%s is a malformated Query tuple" %
+                            (key, kwargs[key]))
+        except BadValueError:
+            raise AttributeError("invalid type for attribute %s" % key)
+
+    # set limit to the proper value
+    if(limit is not None):
+        if(limit > query.count()):
+            limit = query.count()
+    else:
+        limit = query.count()
+
+    # execute the query
+    results = query.fetch(limit)
+
+    # construct instances of the User class for each UserModel returned
+    users = []
+    for user_model in results:
+        user = User()
+        user._User__UserModel = user_model # pylint: disable=C0103,W0201
+        users.append(user)
+
+    return users
+
+        
--- src/tests/test_accounts/test_accounts.py
+++ src/tests/test_accounts/test_accounts.py
@@ -13,8 +13,6 @@ print sys.path
 
 import unittest
 
-from google.appengine.ext import db
-
 from tests.testHarness import AppEngineTestCase as TestCase
 
 import application.accounts.accounts as accounts
@@ -67,7 +65,7 @@ class CreateUserTestCase(TestCase): # pylint: disable=R0904
         except AttributeError:
             pass
 
-    def test_error_on_nonmodifiable_kwargs(self):
+    def test_error_on_nonmod_kwargs(self):
         """This method tests whether the create_user
         function will throw an error when a nonmodifiable
         field of kwargs is tried to be set via the kwargs
@@ -78,7 +76,7 @@ class CreateUserTestCase(TestCase): # pylint: disable=R0904
                                  'jjs11',
                                  'password',
                                  hash='hash')
-            self.fail('Should have thrown a BadValueError')
+            self.fail('Should have thrown an AttributeError')
         except AttributeError:
             pass
 
@@ -94,8 +92,8 @@ class CreateUserTestCase(TestCase): # pylint: disable=R0904
                                 'jjs11',
                                 'password',
                                 mname=55)
-            self.fail('Should have thrown a BadValueError')
-        except db.BadValueError:
+            self.fail('Should have thrown an AttributeError')
+        except AttributeError:
             pass
 
     def test_non_unique_cwruid(self):
@@ -118,6 +116,109 @@ class CreateUserTestCase(TestCase): # pylint: disable=R0904
         except AttributeError:
             pass
 
+class FindUsersTestCase(TestCase): # pylint: disable=R0904
+    """This class contains methods that
+    test the application.accounts.accounts.find_users
+    method"""
+
+    def setUp(self): # pylint: disable=C0103
+        """This method calls the normal
+        setUp method provided by the AppEngineTestCase.
+        It also sets up multiple test users that
+        are queried against to test the find_users
+        method"""
+        # do the normal setup
+        super(FindUsersTestCase, self).setUp()
+
+        # create some test users
+        self.users = []
+        self.users.append(accounts.create_user('Eric',
+                                               'Cartman',
+                                               'exc',
+                                               'password'))
+        self.users.append(accounts.create_user('Randy',
+                                               'Marsh',
+                                               'rxm',
+                                               'password'))
+        self.users.append(accounts.create_user('Stan',
+                                               'Marsh',
+                                               'sxm1',
+                                               'password',
+                                               big=self.users[1].key()))
+        self.users.append(accounts.create_user('Kyle',
+                                               'Broflovski',
+                                               'kxb1',
+                                               'password'))
+
+    def test_return_all_users(self):
+        """This method tests whether find_users
+        will return all users with no parameters
+        """
+        users = accounts.find_users()
+
+        self.assertEqual(len(users), 4)
+
+        for user in users:
+            self.assertIn(user, self.users)
+
+    def test_return_limited(self):
+        """This method tests whether the find_users
+        will return a limited amount of users
+        when limit is specified
+        """
+        users = accounts.find_users(1)
+
+        self.assertEqual(len(users), 1)
+
+        self.assertIn(users[0], self.users)
+
+    def test_return_cwruid_match(self):
+        """This method tests whether the find_users
+        method will return the user matching the
+        cwruid specified
+        """
+        users = accounts.find_users(cwruid=('=','rxm'))
+
+        self.assertEqual(len(users), 1)
+
+        self.assertEqual(users[0], self.users[1])
+
+    def test_return_multiple_match(self):
+        """This method tests whether multiple
+        users will be returned when multiple
+        users are matched
+        """
+        users = accounts.find_users(lname=('=','Marsh'))
+
+        self.assertEqual(len(users), 2)
+
+        for user in users:
+            self.assertIn(user, self.users[1:3])
+
+    def test_invalid_queries(self):
+        """This method tests whether errors
+        are raised when invalid queries are
+        issued"""
+
+        try:
+            accounts.find_users(fake=('=','fake'))
+            self.fail("Should have raised an error")
+        except AttributeError:
+            pass
+
+        try:
+            accounts.find_users(limit=-1)
+            self.fail("Should have raised an error")
+        except TypeError:
+            pass
+
+        try:
+            accounts.find_users(lname=('='))
+            self.fail("Should have raised an error")
+        except TypeError:
+            pass
+
+                          
 class UserTestCase(TestCase): # pylint: disable=R0904
     """This class contains methods that
     test the User class in the accounts.accounts
