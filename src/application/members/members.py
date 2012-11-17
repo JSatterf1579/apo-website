5ba09a8962a0e09e2067e63c149e2d6c5bf2f5a2
Devin Schwab <digidevin@gmail.com>
2012-11-17 09:29:51 -0500

    Added the get_avatar_url function

--- src/application/members/members.py
+++ src/application/members/members.py
@@ -7,3 +7,26 @@ It utilizes the methods found in the accounts package.
 
 .. moduleauthor:: Devin Schwab <dts34@case.edu>
 """
+
+import urllib, hashlib
+
+def get_avatar_url(email, size=100, default='/static/img/avatar.png'):
+    """
+    This function takes a Gravatar email address
+    and returns the gravatar image url
+
+    :param email: The Gravatar email address
+    :type email: unicode
+
+    :param size: Size of the returned avatar image
+    :type size: int
+
+    :param default: Default avatar url
+    :type default: unicode
+    """
+
+    gravatar_url = "http://www.gravatar.com/avatar/"
+    gravatar_url += hashlib.md5(email.lower()).hexdigest() + "?"
+    gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+
+    return gravatar_url
