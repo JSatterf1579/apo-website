e98e70432efe6f072c03848b3167ef862b930023
Devin Schwab <digidevin@gmail.com>
2012-11-17 09:17:50 -0500

    Added the basic forms needed for the members module. Also moved the reset password view to the accounts.views module

---
+++ src/application/members/forms.py
@@ -0,0 +1,45 @@
+"""This module contains the forms for the members package
+
+.. module:: application.members.forms
+
+.. moduleauthor:: Devin Schwab <dts34@case.edu>
+"""
+
+from flaskext import wtf
+from flaskext.wtf import validators
+
+class RoleForm(wtf.Form):
+    name = wtf.TextField('Role Name', [validators.Required()])
+
+class AddressForm(wtf.Form):
+    addrName = wtf.TextField('Name', [validators.Optional()])
+    street1 = wtf.TextField('Street', [validators.Required()])
+    street2 = wtf.TextField('', [validators.Optional()])
+    city = wtf.TextField('City', [validators.Required()])
+    state = wtf.TextField('State', [validators.Required()])
+    zip = wtf.IntegerField('Zip', [validators.Required()])
+
+class PhoneNumberForm(wtf.Form):
+    phoneName = wtf.TextField('Name', [validators.Optional()])
+    phoneNumber = wtf.TextField('Phone Number', [validators.Required()])
+
+class EmailAddressForm(wtf.Form):
+    emailName = wtf.TextField('Name', [validators.Optional()])
+    emailAddress = wtf.TextField('Email', [validators.Email()])
+
+class SearchUserForm(wtf.Form):
+    fName = wtf.TextField('First Name', [validators.Optional()])
+    mName = wtf.TextField('Middle Name', [validators.Optional()])
+    lName = wtf.TextField('Last Name', [validators.Optional()])
+
+class CreateUserForm(wtf.Form):
+    fName = wtf.TextField('First Name', [validators.Required()])
+    mName = wtf.TextField('Middle Name', [validators.Optional()])
+    lName = wtf.TextField('Last Name', [validators.Required()])
+    cwruid = wtf.TextField('CWRU ID', [validators.Required()])
+    family = wtf.TextField('Family Name', [validators.Optional()])
+    big = wtf.TextField('Big CWRU ID', [validators.Optional()])
+    avatar = wtf.TextField('Gravatar email', [validators.Email()])
+    roles = wtf.FieldList(wtf.SelectMultipleField('Role Name', [validators.Optional()]))
+
+
