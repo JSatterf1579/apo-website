3e7d518043c6aff6169606df3955842488710f74
Devin Schwab <digidevin@gmail.com>
2012-11-21 21:26:35 -0500

    Removed a bunch of example code

--- src/application/views.py
+++ src/application/views.py
@@ -13,9 +13,8 @@ from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
 
 from flask import render_template, flash, url_for, redirect, request
 
-from models import ExampleModel
 from decorators import login_required, admin_required
-from forms import ExampleForm
+
 
 import models, forms, accounts, generate_keys
 
@@ -41,69 +40,6 @@ def calendar():
     View for the calendar
     """
     return render_template('calendar/calendar.html')
-
-@app.route('/hello/<username>')
-def say_hello(username):
-    """Contrived example to demonstrate Flask's url routing capabilities
-
-    :rtype: HTML page
-    """
-    return 'Hello %s' % username
-
-
-@app.route('/examples', methods=['GET','POST'])
-@login_required
-def list_examples():
-    """List all examples
-
-    :rtype: HTML page
-    """
-    examples = ExampleModel.all()
-    form = ExampleForm()
-    if form.validate_on_submit():
-        example = ExampleModel(
-            example_name = form.example_name.data,
-            example_description = form.example_description.data,
-            added_by = users.get_current_user()
-        )
-        try:
-            example.put()
-            example_id = example.key().id()
-            flash(u'Example %s successfully saved.' % example_id, 'success')
-            return redirect(url_for('list_examples'))
-        except CapabilityDisabledError:
-            flash(u'App Engine Datastore is currently in read-only mode.', 'info')
-            return redirect(url_for('list_examples'))
-    return render_template('list_examples.html', examples=examples, form=form)
-
-
-@app.route('/examples/delete/<int:example_id>', methods=['POST'])
-@login_required
-def delete_example(example_id):
-    """Delete an example object"""
-    example = ExampleModel.get_by_id(example_id)
-    try:
-        example.delete()
-        flash(u'Example %s successfully deleted.' % example_id, 'success')
-        return redirect(url_for('list_examples'))
-    except CapabilityDisabledError:
-        flash(u'App Engine Datastore is currently in read-only mode.', 'info')
-        return redirect(url_for('list_examples'))
-
-
-@app.route('/admin_only')
-@admin_required
-def admin_only():
-    """This view requires an admin account"""
-    return 'Super-seekrit admin page.'
-
-
-def warmup():
-    """App Engine warmup handler
-    See http://code.google.com/appengine/docs/python/config/appconfig.html#Warming_Requests
-
-    """
-    return ''
 
 ## Error handlers
 # Handle 404 errors
